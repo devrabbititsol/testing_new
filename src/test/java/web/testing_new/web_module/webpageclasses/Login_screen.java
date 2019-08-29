928f1276881ab6891ebe07cb4616e0f4c0ca977e
root <santhosh.annam@devrabbit.com>
2019-08-29 12:20:14 +0530

    commit

--- src/test/java/web/testing_new/web_module/webpageclasses/Login_screen.java
+++ src/test/java/web/testing_new/web_module/webpageclasses/Login_screen.java
@@ -10,7 +10,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class Login_screen extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":85,\"executed_user_id\":85,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbitdev.com/login.php\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"Testing_new\",\"project_description\":\"desc\",\"project_id\":423,\"module_name\":\"web_module\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":716,\"testcase_name\":\"TC_Notifii\",\"testcase_id\":579,\"testset_id\":0,\"executed_timestamp\":-602325373,\"browser_type\":\"chrome\",\"testcase_overwrite\":true,\"client_timezone_id\":\"Asia/Calcutta\"}";
+	public static String primaryInfo  = "{\"user_id\":85,\"executed_user_id\":85,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://www.devrabbitdev.com/login.php\",\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\",\"project_name\":\"Testing_new\",\"project_description\":\"desc\",\"project_id\":0,\"module_name\":\"web_module\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":716,\"testcase_name\":\"TC_Notifii\",\"testcase_id\":579,\"testset_name\":\"TS\",\"testset_id\":85,\"executed_timestamp\":-601651485,\"browser_type\":\"chrome,mozilla\",\"testcase_overwrite\":true,\"client_timezone_id\":\"Asia/Calcutta\"}";
 
 	public static String projectName = "testing_new";
 	public WebDriver driver;
